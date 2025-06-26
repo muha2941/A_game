010172c920b57c70781b96cb862eef4d0610fec2
muha2941 <rabiamuttahhar56@gmail.com>
2025-06-26 12:15:16 +0200

    Add files via upload

--- configuration.py
+++ configuration.py
@@ -1,28 +1,26 @@
-
 WIN_WIDTH,WIN_HEIGHT=800,600
-TILESIZE=24
-FPS =60
+TILESIZE= 32
+FPS=60
 
-PLAYER_LAYER=4
 WATER_LAYER=3
 BLOCKS_LAYER=2
 GROUND_LAYER=1
-
 BLACK=(0,0,0)
+RED=(225,0,0)
 
 tilemap=[
     "BBBBBBBBBBBBBBBBBBBB",
-    "B.....B............B",
-    "B....BB.BB.........B",
-    "B...B.....B........B",
-    "B............BB....B",
-    "B.............BB...B",
-    "B...........WWWWWWWB",
-    "BW....BB...WW..B...B",
-    "B.W....BWWW........B",
-    "B..WWWWWW..........B",
-    "B.......BBBBB......B",
     "B..................B",
+    "B....B......BBB....B",
+    "B..BBB....BBB......B",
     "B..................B",
-    "BBBBBBBBBBBBBBBBBBBB"
+    "B.............BBB..B",
+    "B....B.............B",
+    "B....B...BB........B",
+    "B...BB.............B",
+    "B..........BB......B",
+    "B.......BB.........B",
+    "B...BB.............B",
+    "B........BBB.......B",
+    "BBBBBBBBBBBBBBBBBBBB",
 ]
